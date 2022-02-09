ea51937a8b8dec8de9c89a65e566cb4f85fc83fa
Adam Andrzejewski <adam.andrzejewski@onegini.com>
2022-02-09 21:15:00 +0100

    fix config so tests are run on compile + minor code fixes

--- hotel-reservations/src/main/java/org/example/model/AvailableRooms.java
+++ hotel-reservations/src/main/java/org/example/model/AvailableRooms.java
@@ -1,13 +1,10 @@
 package org.example.model;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AvailableRooms {
 
   private int premiumRooms;
--- hotel-reservations/src/test/java/org/example/service/UsageCalculationServiceTest.java
+++ hotel-reservations/src/test/java/org/example/service/UsageCalculationServiceTest.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 
 class UsageCalculationServiceTest {
 
-  private UsageCalculationService service;
+  private UsageCalculationService service = new UsageCalculationService();
 
   // initial input [23, 45, 155, 374, 22, 99.99, 100, 101, 115, 209]
   @Test
